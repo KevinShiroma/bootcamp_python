866cc98828e8e75943df6efb43e4042528f27da5
Kevin <makoto.shiroma@hotmail.com>
2024-12-28 10:05:06 -0300

    aula10 arrumada

--- aula10_poo_sql_model/main.py
+++ aula10_poo_sql_model/main.py
@@ -1,1 +1,10 @@
-print("Hello World")
+from typing import Optional
+
+from sqlmodel import Field, SQLModel
+
+
+class Hero(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    secret_name: str
+    age: Optional[int] = None
